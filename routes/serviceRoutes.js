5a306b2edd3e46dabf497fac849f6a74ab36544f
PankajWSC07 <Pankaj.makvana@weservecodes.com>
2026-03-05 15:29:39 +0530

    update service route

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
 import serviceController from "../controllers/serviceController.js";
+import adminController from "../controllers/adminController.js";
 
 const router = express.Router();
 
+// Public endpoints - accessible to everyone (no auth required)
+router.get("/categories", adminController.getAllCategories);
+router.get("/category/:id", adminController.getCategoryById);
+
+// Service endpoints
 router.get("/", serviceController.getAllServices);
 router.get("/available", serviceController.getAvailableServices);
 router.get("/:id", serviceController.getServiceById);
